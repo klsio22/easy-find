4f6ad14938319db62b53a25293081edecfdf642d
klesio <klesio.nascimento@penseavanti.com.br>
2024-04-14 02:51:54 -0300

    feat(login): inicializando tela de login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
 })
 export class AppComponent {
   title = 'easy-find';
---
+++ src/app/components/pages/login/login.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      // Lógica de autenticação aqui
+      console.log(this.loginForm.value);
+    }
+  }
+}
