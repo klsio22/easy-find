9f3293296a1e096682380c7870fd624223d1f9fa
klesio <klesio.nascimento@penseavanti.com.br>
2024-04-14 23:59:03 -0300

    feat(header): create header page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
 })
 export class AppComponent {
-  title = 'easy-find';
 }
