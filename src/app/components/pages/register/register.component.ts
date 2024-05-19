4bbd5a7170b48c922ce78755b5197495e155b58f
klesio <klesio.nascimento@penseavanti.com.br>
2024-05-19 15:53:43 -0300

    feat(register): registration form authentication

--- src/app/components/pages/register/register.component.ts
+++ src/app/components/pages/register/register.component.ts
@@ -1,23 +1,64 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+  AbstractControl,
+  ValidationErrors,
+} from '@angular/forms';
+
+interface RegisterData {
+  email: string;
+  password: string;
+}
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './register.component.html',
 })
 export class RegisterComponent {
-  registerForm: FormGroup;
+  protected registerForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
-    this.registerForm = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required],
-      confirmPassword: ['', Validators.required]
-    });
+  constructor(private formBuilder: FormBuilder, private router: Router) {
+    this.registerForm = this.formBuilder.group(
+      {
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        confirmPassword: ['', Validators.required],
+      },
+      {
+        validators: this.passwordMatch,
+      },
+    );
   }
 
-  onSubmit() {
+  protected onSubmit() {
+    if (this.registerForm.valid) {
+      this.saveToStorage(this.registerForm.value);
+      this.router.navigate(['/home']);
+      console.log('Form data:', this.registerForm.value);
+    }
+  }
+
+  passwordMatch(control: AbstractControl): ValidationErrors | null {
+    const password = control.get('password')?.value;
+    const confirmPassword = control.get('confirmPassword')?.value;
+
+    if (password !== confirmPassword) {
+      control.get('confirmPassword')?.setErrors({ passwordMatch: true });
+      return { passwordMatch: true };
+    } else {
+      control.get('confirmPassword')?.setErrors(null);
+      return null;
+    }
+  }
+
+  private saveToStorage(data: RegisterData) {
+    localStorage.setItem('registerData', JSON.stringify(data));
   }
 }
