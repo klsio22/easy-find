b65e4db0a032c4ec631eec0cfd5a8df5e28506cb
klesio <klesio.nascimento@penseavanti.com.br>
2024-05-31 01:47:59 -0300

    feat(login): Configuring login authentication in firebase

--- src/app/components/pages/login/login.component.ts
+++ src/app/components/pages/login/login.component.ts
@@ -6,6 +6,9 @@ import {
   Validators,
 } from '@angular/forms';
 
+import { Auth, GoogleAuthProvider, signInWithPopup } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -15,10 +18,28 @@ import {
 export class LoginComponent {
   loginForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private afAuth: Auth,
+    private router: Router,
+  ) {
+    this.router = router;
+    this.afAuth = afAuth;
     this.loginForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required],
     });
+  }
+
+  async loginWithGoogle() {
+    try {
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(this.afAuth, provider);
+      console.log('Usuário logado:', result.user);
+      this.router.navigate(['/home']);
+    } catch (error) {
+      console.error('Erro ao fazer login:', error);
+    }
   }
 }
